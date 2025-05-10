86392369f1435349236542d03dbafca5dd27e575
ghrjeon <ghrjeon@gmail.com>
2025-05-10 00:11:32 -0400

    update chrome

--- scrape/fetch_cryptojobscom.py
+++ scrape/fetch_cryptojobscom.py
@@ -34,6 +34,9 @@ class CryptoJobsComFetcher:
         options.add_argument('--headless')
         options.add_argument('--no-sandbox')
         options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-extensions')
+        options.add_argument('--disable-software-rasterizer')
         
         self.logger.info("Initializing CryptoJobsComFetcher with headless Chrome")
         
