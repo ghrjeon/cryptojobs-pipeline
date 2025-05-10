a1d30c6d1d1d12f81005006d19a323dad43df518
ghrjeon <ghrjeon@gmail.com>
2025-05-10 00:08:01 -0400

    update chrome driver

--- scrape/fetch_cryptojobscom.py
+++ scrape/fetch_cryptojobscom.py
@@ -46,8 +46,8 @@ class CryptoJobsComFetcher:
                 # Check if we're in GitHub Actions environment
                 if 'GITHUB_ACTIONS' in os.environ:
                     # Use ChromeDriver installed by the GitHub Action
-                    self.logger.info("Running in GitHub Actions, using pre-installed ChromeDriver")
-                    service = Service('chromedriver')
+                    self.logger.info("Running in GitHub Actions, using system ChromeDriver")
+                    service = Service('/usr/local/bin/chromedriver')
                 else:
                     # Local development - use ChromeDriverManager
                     self.logger.info("Running locally, using ChromeDriverManager")
