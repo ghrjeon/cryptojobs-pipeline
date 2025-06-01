4034f2d15e05d74676b3f334ada6c72c97d6e8c9
ghrjeon <ghrjeon@gmail.com>
2025-05-31 20:16:40 -0400

    update chrome

--- scrape/fetch_cryptojobscom.py
+++ scrape/fetch_cryptojobscom.py
@@ -55,7 +55,7 @@ class CryptoJobsComFetcher:
                         os.system("rm /usr/local/bin/chromedriver")
                     
                     self.logger.info("Installing specific ChromeDriver version")
-                    service = Service(ChromeDriverManager(version='137').install())
+                    service = Service(ChromeDriverManager(version='136.0.7103.113').install())
                 
                 self.logger.info("ChromeDriver setup completed")
                 
