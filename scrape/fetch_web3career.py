61b017a59c87f33fbca11b715f311d0cef9bb4e6
ghrjeon <ghrjeon@gmail.com>
2025-11-05 07:22:30 -0500

    update chrome driver

--- scrape/fetch_web3career.py
+++ scrape/fetch_web3career.py
@@ -56,7 +56,7 @@ class Web3CareerFetcher:
                         os.system("rm /usr/local/bin/chromedriver")
                     
                     self.logger.info("Installing specific ChromeDriver version")
-                    service = Service(ChromeDriverManager(version='140').install())
+                    service = Service(ChromeDriverManager(version='142.0.7444.59').install())
                 
                 self.logger.info("ChromeDriver setup completed")
                 
